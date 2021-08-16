d5d960b28cc2816812bf39c08adacf4f802f9f2f
pengmin <pengmin0115@163.com>
2021-08-16 19:21:38 +0800

    netty学习开始;

---
+++ src/main/java/com/spider/netty/scard/DiscardServer.java
@@ -0,0 +1,53 @@
+package com.spider.netty.scard;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author: pengmin
+ * @date: 2021/8/16 19:10
+ */
+public class DiscardServer {
+
+    private Integer port;
+
+    public DiscardServer(Integer port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception{
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel socketChannel) throws Exception {
+                    socketChannel.pipeline().addLast(new DiscardServerHandler());
+                }
+            }).option(ChannelOption.SO_BACKLOG,128).option(ChannelOption.SO_KEEPALIVE,true);
+            ChannelFuture sync = bootstrap.bind(port).sync();
+            sync.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+        new DiscardServer(port).run();
+    }
+
+}
---
+++ src/main/java/com/spider/netty/scard/DiscardServerHandler.java
@@ -0,0 +1,29 @@
+package com.spider.netty.scard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author: pengmin
+ * @date: 2021/8/16 19:07
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("===  link ===");
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+    }
+}
